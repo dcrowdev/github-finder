a0eec3f2326f02698a85e016ef9213c099991f18
Dylan Crowley <dcrowdev1025@gmail.com>
2023-01-12 14:15:38 -0500

    deployment

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+  const initialSate = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialSate);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
